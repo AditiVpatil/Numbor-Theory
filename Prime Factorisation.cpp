c5eec36977353c4a0113a1dd19ca280599aa90ad
Aditi Patil <patiladiti2206@gmail.com>
2024-12-03 23:11:55 +0530

    Update Prime Factorisation.cpp

--- Prime Factorisation.cpp
+++ Prime Factorisation.cpp
@@ -1,19 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>>
+
 using namespace std;
-int main()
-{
-int n;
-cin>>n;
-int count=0;
-for(int i=1;i<=n;i++){
-    if(n%i==0)
-    count++;
-}
-if (count>2){
-cout<<n<<"not prime"<<endl;
-}
-else{
-cout<<n<<"is prime";
-}
-return 0;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (n % 2 == 0) {
+        cout << 2 << " ";
+        n = n / 2;
+    }
+
+    // factors from 3 to square root n
+    for (int i = 3; i*i <= n; i += 2) {
+        
+        while (n % i == 0) {
+            cout << i << " ";
+            n = n / i;
+        }
+    }
+
+    
+    if (n > 2) {
+        cout << n << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
